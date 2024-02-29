ef1cb1bc1feabe038ea1c465b0e83e8d7cb57ee1
Vishal <vvishalpatidar143@gmail.com>
2024-02-29 12:09:41 +0530

    Create active_storage migration & gem of image upload for person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,4 @@
 class Person < ApplicationRecord
+  has_one_attached :image
   has_many :quotes
 end
--- db/migrate/20240228090247_create_people.rb
+++ db/migrate/20240228090247_create_people.rb
@@ -2,7 +2,6 @@ class CreatePeople < ActiveRecord::Migration[7.1]
   def change
     create_table :people do |t|
       t.string :name
-      t.string :image
 
       t.timestamps
     end
